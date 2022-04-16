a1293ff841323c645f477cdf169e70801aabaac2
Rosana <rosanatsf@gmail.com>
2022-04-16 17:38:37 -0300

    cSharpDio

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Projetinho
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
