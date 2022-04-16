347acba31dcef3946ca6780cc817dd9c7ee0a6ff
Rosana <rosanatsf@gmail.com>
2022-04-16 17:45:04 -0300

    CSharpDio

---
+++ Aula3ref/Program.cs
@@ -0,0 +1,16 @@
+﻿using static System.Console;
+
+Pessoa p1 = new Pessoa();
+
+p1.Nome = "Penélope";
+p1.Idade = 5;
+p1.EnderecoPessoa = new Endereco()
+{
+    Logradouro = "Rua do Manda-Chuva",
+    Numero = 3,
+    CEP = "0456301",
+    Cidade = "São Paulo",
+};
+
+    WriteLine("Fim");
+
